6e431fba4154e25fc189d5a892593869eb05af63
Zhijing Hu <qq906555703@gmail.com>
2021-10-10 16:50:36 +0800

    fix a bug in "user/pingpong.c"

--- user/pingpong.c
+++ user/pingpong.c
@@ -16,19 +16,19 @@ int main(int argc, char *argv[]) {
 
   pipe(p);
   if (fork() == 0) {
-    read(p[0], &buf, 1);
+    read(p[0], &buf, 4);
     if (buf == ping) {
       // received ping, respond pong
       fprintf(1, "%d: received ping\n", getpid());
-      write(p[1], &pong, 1);
+      write(p[1], &pong, 4);
       exit(0);
     } else {
       fprintf(2, "error receiving ping");
       exit(1);
     }
   } else {
-    write(p[1], &ping, 1);
-    read(p[0], &buf, 1);
+    write(p[1], &ping, 4);
+    read(p[0], &buf, 4);
     if (buf == pong) {
       fprintf(1, "%d: received pong\n", getpid());
       exit(0);
