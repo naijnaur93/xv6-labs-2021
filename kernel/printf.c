5e1719d490a17797a0aedc7d67b8fa430fcbe2b5
Zhijing Hu <qq906555703@gmail.com>
2021-11-08 14:41:25 +0800

    Task risc-v assembly and Backtrace Complete

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,16 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void 
+backtrace(void) 
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  while (PGROUNDUP(fp) != *((uint64*)(fp-16)))
+  {
+    printf("%p\n", *((uint64*)(fp - 8)));
+    fp = *((uint64 *)(fp - 16));
+  }
+  printf("%p\n", *((uint64 *)(fp - 8)));
+}
